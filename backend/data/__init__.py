4517846e1c428f14164849c8aee98556c8765f4e
mrjleo <mrjleo@users.noreply.github.com>
2025-01-16 13:07:14 +0100

    backend update

--- backend/data/__init__.py
+++ backend/data/__init__.py
@@ -1,58 +1,71 @@
 from abc import ABC, abstractmethod
 
 import ir_datasets
+from fastapi import HTTPException
 from pydantic import BaseModel
 
 
-class CorpusInfo(BaseModel):
+class CorpusDescription(BaseModel):
     name: str
     num_documents: int
+    searchers: list[str] = []
 
 
-class DatasetInfo(BaseModel):
+class DatasetDescription(BaseModel):
     name: str
     corpus: str
     num_queries: int
-    num_qrels: int
+    num_qrels: int = 0
+
+
+class Query(BaseModel):
+    id: str
+    text: str
+    description: str | None = None
+    metadata: dict[str, str] = {}
+
+
+class Document(BaseModel):
+    id: str
+    text: str
+    title: str | None = None
+    metadata: dict[str, str] = {}
 
 
 class SearchHit(BaseModel):
     document_id: str
-    score: float
+    score: float | None
 
 
 class SearchResult(BaseModel):
     query: str
+    query_id: str | None = None
     result: list[SearchHit]
 
 
 class DataStore(ABC):
     @abstractmethod
-    def available_corpora(self) -> list[CorpusInfo]:
+    def available_corpora(self) -> list[CorpusDescription]:
         pass
 
     @abstractmethod
-    def available_datasets(self) -> list[DatasetInfo]:
+    def available_datasets(self) -> list[DatasetDescription]:
         pass
 
     @abstractmethod
-    def list_queries(
-        self, dataset: str, start_index: int, num: int
-    ) -> list[dict[str, str]]:
+    def list_queries(self, dataset: str, start_index: int, num: int) -> list[Query]:
         pass
 
     @abstractmethod
-    def list_documents(
-        self, dataset: str, start_index: int, num: int
-    ) -> list[dict[str, str]]:
+    def list_documents(self, corpus: str, start_index: int, num: int) -> list[Document]:
         pass
 
     @abstractmethod
-    def get_query(self, dataset: str, q_id: str) -> dict[str, str]:
+    def get_query(self, dataset: str, q_id: str) -> Query:
         pass
 
     @abstractmethod
-    def get_document(self, dataset: str, doc_id: str) -> dict[str, str]:
+    def get_document(self, corpus: str, doc_id: str) -> Document:
         pass
 
 
@@ -72,52 +85,44 @@ class TestDataStore(DataStore):
     ds = {
         "beir/fiqa/test": ir_datasets.load("beir/fiqa/test"),
     }
+    corpora = {
+        "beir/fiqa": ir_datasets.load("beir/fiqa"),
+    }
 
-    def available_corpora(self) -> list[CorpusInfo]:
-        result = []
-        for ds_name, ds in self.ds.items():
-            result.append(
-                {
-                    "name": ds_name.split("/")[0],
-                    "num_documents": ds.docs_count(),
-                }
-            )
-        return result
-
-    def available_datasets(self) -> list[DatasetInfo]:
-        result = []
-        for ds_name, ds in self.ds.items():
-            result.append(
-                {
-                    "name": ds_name,
-                    "corpus": ds_name.split("/")[0],
-                    "num_queries": ds.queries_count(),
-                    "num_qrels": ds.qrels_count(),
-                }
-            )
-        return result
-
-    def list_queries(
-        self, dataset: str, start_index: int, num: int
-    ) -> list[dict[str, str]]:
-        return [x._asdict() for x in self.ds[dataset].queries_iter()][
-            start_index : start_index + num
-        ]
-
-    def list_documents(
-        self, dataset: str, start_index: int, num: int
-    ) -> list[dict[str, str]]:
+    def available_corpora(self) -> list[CorpusDescription]:
         return [
-            x._asdict()
-            for x in self.ds[dataset].docs_iter()[start_index : start_index + num]
+            CorpusDescription(name=name, num_documents=corpus.docs_count())
+            for name, corpus in self.corpora.items()
         ]
 
-    def get_query(self, dataset: str, q_id: str) -> dict[str, str]:
+    def available_datasets(self) -> list[DatasetDescription]:
+        return [
+            DatasetDescription(
+                name=ds_name,
+                corpus=ds_name.split("/")[0],
+                num_queries=ds.queries_count(),
+                num_qrels=ds.qrels_count(),
+            )
+            for ds_name, ds in self.ds.items()
+        ]
+
+    def list_queries(self, dataset: str, start_index: int, num: int) -> list[Query]:
+        return [
+            Query(id=x.query_id, text=x.text) for x in self.ds[dataset].queries_iter()
+        ][start_index : start_index + num]
+
+    def list_documents(self, corpus: str, start_index: int, num: int) -> list[Document]:
+        return [
+            Document(id=x.doc_id, text=x.text)
+            for x in self.ds[corpus].docs_iter()[start_index : start_index + num]
+        ]
+
+    def get_query(self, dataset: str, q_id: str) -> Query:
         for query in self.ds[dataset].queries_iter():
             if query.query_id == q_id:
-                return query._asdict()
-        return {}
+                return Query(id=query.query_id, text=query.text)
+        raise HTTPException(404)
 
-    def get_document(self, dataset: str, doc_id: str) -> dict[str, str]:
-        docstore = self.ds[dataset].docs_store()
-        return docstore.get(doc_id)._asdict()
+    def get_document(self, corpus: str, doc_id: str) -> Document:
+        doc = self.ds[corpus].docs_store().get(doc_id)
+        return Document(id=doc.doc_id, text=doc.text)
