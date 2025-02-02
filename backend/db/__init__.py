0be3ba0318f2a870a7788e46bfba3c65c401bb55
mrjleo <mrjleo@users.noreply.github.com>
2025-02-02 14:14:41 +0100

    add qrels association table

--- backend/db/__init__.py
+++ backend/db/__init__.py
@@ -4,6 +4,7 @@ from typing import TYPE_CHECKING
 
 from litestar.exceptions import ClientException
 from litestar.status_codes import HTTP_409_CONFLICT
+from sqlalchemy import Column, ForeignKeyConstraint, Integer, Table
 from sqlalchemy.engine import URL
 from sqlalchemy.exc import IntegrityError
 from sqlalchemy.ext.asyncio import (
@@ -11,7 +12,7 @@ from sqlalchemy.ext.asyncio import (
     async_sessionmaker,
     create_async_engine,
 )
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
 
 if TYPE_CHECKING:
     from collections.abc import AsyncGenerator
@@ -24,19 +25,36 @@ class Base(DeclarativeBase):
     pass
 
 
+qrels_table = Table(
+    "qrels",
+    Base.metadata,
+    Column("query_id"),
+    Column("dataset"),
+    Column("document_id"),
+    Column("corpus"),
+    ForeignKeyConstraint(["query_id", "dataset"], ["queries.id", "queries.dataset"]),
+    ForeignKeyConstraint(
+        ["document_id", "corpus"], ["documents.id", "documents.corpus"]
+    ),
+    Column("relevance", Integer()),
+)
+
+
 class Query(Base):
     __tablename__ = "queries"
     id: Mapped[str] = mapped_column(primary_key=True)
     dataset: Mapped[str] = mapped_column(primary_key=True)
-    text: Mapped[str]
+    text: Mapped[str] = mapped_column()
     description: Mapped[str] = mapped_column(nullable=True)
+
+    rel_docs: Mapped[list["Document"]] = relationship(secondary=qrels_table)
 
 
 class Document(Base):
     __tablename__ = "documents"
     id: Mapped[str] = mapped_column(primary_key=True)
     corpus: Mapped[str] = mapped_column(primary_key=True)
-    text: Mapped[str]
+    text: Mapped[str] = mapped_column()
     title: Mapped[str] = mapped_column(nullable=True)
 
 
