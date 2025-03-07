ee1f01d3da10ac488a4069c10f5302405cf3affb
mrjleo <mrjleo@users.noreply.github.com>
2025-03-07 17:42:51 +0100

    add simple search interface

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -3,7 +3,17 @@
   import type { Corpus } from "$lib/types";
 
   let { data }: PageProps = $props();
+
   let selected_corpus: Corpus | undefined = $state();
+  let search: string = $state("");
+  let target: string = $derived(
+    selected_corpus
+      ? "/search/" +
+          selected_corpus.name +
+          "?" +
+          new URLSearchParams({ q: search })
+      : "/"
+  );
 </script>
 
 <label for="cars">Choose a corpus:</label>
@@ -15,3 +25,7 @@
 <p>
   selected corpus: {selected_corpus ? selected_corpus.name : ""}
 </p>
+<input type="text" bind:value={search} />
+<a href={target}>
+  <button>search</button>
+</a>
