41ce46236c9417e358b31fec7135b7bd7415cfc1
mrjleo <mrjleo@users.noreply.github.com>
2025-03-21 14:56:14 +0100

    add corpus name

--- frontend/src/routes/search/[corpus]/+page.svelte
+++ frontend/src/routes/search/[corpus]/+page.svelte
@@ -1,27 +1,36 @@
 <script lang="ts">
   import type { PageProps } from "./$types";
   import Fa from "svelte-fa";
-  import { faFileLines, faHashtag } from "@fortawesome/free-solid-svg-icons";
+  import {
+    faBook,
+    faFileLines,
+    faHashtag,
+  } from "@fortawesome/free-solid-svg-icons";
 
   let { data }: PageProps = $props();
 </script>
 
 <ul class="list bg-base-100 rounded-box shadow-md">
-  <li class="p-4 pb-2 text-xs">Search results</li>
+  <li class="p-4 text-xs">Search results</li>
 
   {#each data["hits"] as hit, index}
     <li class="list-row">
       <div>
-        <div class="flex flex-row gap-2">
-          <div class="tooltip" data-tip="Score: {hit.score}">
-            <p class="badge badge-outline badge-primary font-bold">
-              <Fa icon={faHashtag} />
-              {index + 1}
-            </p>
-          </div>
-          <p class="badge badge-outline">
+        <div class="join">
+          <p
+            class="badge badge-primary join-item tooltip"
+            data-tip="Score: {hit.score}"
+          >
+            <Fa icon={faHashtag} size={"sm"} />
+            <span class="-ml-1.5">{index + 1}</span>
+          </p>
+          <p class="badge join-item">
             <Fa icon={faFileLines} />
             {hit.id}
+          </p>
+          <p class="badge join-item">
+            <Fa icon={faBook} />
+            {hit.corpus_name}
           </p>
         </div>
         <p class="my-2">{hit.text}</p>
