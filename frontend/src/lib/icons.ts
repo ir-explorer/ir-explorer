f507e461cfaf652cbb2076cb8ed73f4970e687f4
mrjleo <mrjleo@users.noreply.github.com>
2025-03-23 13:40:27 +0100

    update corpus icon

--- frontend/src/lib/icons.ts
+++ frontend/src/lib/icons.ts
@@ -1,11 +1,11 @@
 import {
-  faBook,
   faFileLines,
+  faFolderClosed,
   faMagnifyingGlass,
   faSliders,
 } from "@fortawesome/free-solid-svg-icons";
 
 export const searchIcon = faMagnifyingGlass;
-export const corpusIcon = faBook;
+export const corpusIcon = faFolderClosed;
 export const documentIcon = faFileLines;
 export const settingsIcon = faSliders;
