0830de32ed3e8cf00372b9638d18a41e1e24fda2
mrjleo <mrjleo@users.noreply.github.com>
2025-03-23 13:16:16 +0100

    make logo text non-selectable

--- frontend/src/lib/components/Logo.svelte
+++ frontend/src/lib/components/Logo.svelte
@@ -3,8 +3,8 @@
   import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 </script>
 
-<p class="flex font-serif text-2xl">
-  <span class="mr-1 text-base-100 border bg-primary px-1.5 rounded">IR</span
+<p class="flex font-serif text-2xl/tight select-none">
+  <span class="mr-1 text-base-100 border bg-primary px-1 rounded">IR</span
   >expl<span class="mt-2 -mr-1 text-primary">
     <Fa icon={faMagnifyingGlass} />
   </span>rer
