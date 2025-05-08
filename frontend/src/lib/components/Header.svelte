74c0b00b5a793dd593e6bd22376e0c9f88d2dbaf
mrjleo <mrjleo@users.noreply.github.com>
2025-05-08 13:48:43 +0200

    make Header component more flexible

--- frontend/src/lib/components/Header.svelte
+++ frontend/src/lib/components/Header.svelte
@@ -3,21 +3,31 @@
   import Logo from "./Logo.svelte";
   import BusyIndicator from "./BusyIndicator.svelte";
 
-  let { children } = $props();
+  let { start = null, center = null, end = null } = $props();
 </script>
 
 <div
   class="fixed top-0 z-1 navbar border-b border-base-300 bg-base-200/75 px-8 backdrop-blur-sm">
-  <div class="navbar-start gap-4">
-    <Menu />
-    <a href="/">
-      <Logo />
-    </a>
+  <div class="navbar-start">
+    {#if start}
+      {@render start()}
+    {:else}
+      <div class="flex gap-4">
+        <Menu />
+        <a href="/">
+          <Logo />
+        </a>
+      </div>
+    {/if}
   </div>
   <div class="navbar-center">
-    {@render children()}
+    {@render center?.()}
   </div>
   <div class="navbar-end">
-    <BusyIndicator />
+    {#if end}
+      {@render end()}
+    {:else}
+      <BusyIndicator />
+    {/if}
   </div>
 </div>
--- frontend/src/routes/browse/+layout.svelte
+++ frontend/src/routes/browse/+layout.svelte
@@ -13,38 +13,40 @@
 </script>
 
 <Header>
-  <div class="breadcrumbs text-sm">
-    <ul>
-      <li><a href="/browse"><Fa icon={browseIcon} />Browse</a></li>
-      {#if data.corpusName}
-        <li>
-          <a href="/browse/{data.corpusName}"
-            ><Fa icon={corpusIcon} />{data.corpusName}
-          </a>
-        </li>
-      {/if}
-
-      {#if data.datasetName}
-        <li>
-          <a href="/browse/{data.corpusName}/{data.datasetName}"
-            ><Fa icon={datasetIcon} />{data.datasetName}
-          </a>
-        </li>
-        {#if data.queryID}
+  {#snippet center()}
+    <div class="breadcrumbs text-sm">
+      <ul>
+        <li><a href="/browse"><Fa icon={browseIcon} />Browse</a></li>
+        {#if data.corpusName}
           <li>
-            <a
-              href="/browse/{data.corpusName}/{data.datasetName}?query_id={data.queryID}"
-              ><Fa icon={queryIcon} />{data.queryID}</a>
+            <a href="/browse/{data.corpusName}"
+              ><Fa icon={corpusIcon} />{data.corpusName}
+            </a>
           </li>
         {/if}
-      {:else if data.documentID}
-        <li>
-          <a href="/browse/{data.corpusName}?document_id={data.documentID}"
-            ><Fa icon={documentIcon} />{data.documentID}</a>
-        </li>
-      {/if}
-    </ul>
-  </div>
+
+        {#if data.datasetName}
+          <li>
+            <a href="/browse/{data.corpusName}/{data.datasetName}"
+              ><Fa icon={datasetIcon} />{data.datasetName}
+            </a>
+          </li>
+          {#if data.queryID}
+            <li>
+              <a
+                href="/browse/{data.corpusName}/{data.datasetName}?query_id={data.queryID}"
+                ><Fa icon={queryIcon} />{data.queryID}</a>
+            </li>
+          {/if}
+        {:else if data.documentID}
+          <li>
+            <a href="/browse/{data.corpusName}?document_id={data.documentID}"
+              ><Fa icon={documentIcon} />{data.documentID}</a>
+          </li>
+        {/if}
+      </ul>
+    </div>
+  {/snippet}
 </Header>
 <div class="mx-4 mt-20 mb-4 max-w-full">
   <div class="mx-auto max-w-5xl">
