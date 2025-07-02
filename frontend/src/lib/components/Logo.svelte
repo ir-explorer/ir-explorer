f1a81b5a2b0173b4fb47164724de6a0a63ed7d8e
mrjleo <mrjleo@users.noreply.github.com>
2025-07-02 10:20:22 +0200

    invert logo in dark mode

--- frontend/src/lib/components/Logo.svelte
+++ frontend/src/lib/components/Logo.svelte
@@ -3,7 +3,13 @@
 </script>
 
 {#if small}
-  <img src="/logo-sm.svg" alt="IR explorer" class="h-6" />
+  <img
+    src="/logo-sm.svg"
+    alt="IR explorer"
+    class="h-6 dark:hue-rotate-180 dark:invert" />
 {:else}
-  <img src="/logo.svg" alt="IR explorer" class="h-16 md:h-20 lg:h-24" />
+  <img
+    src="/logo.svg"
+    alt="IR explorer"
+    class="h-16 md:h-20 lg:h-24 dark:hue-rotate-180 dark:invert" />
 {/if}
