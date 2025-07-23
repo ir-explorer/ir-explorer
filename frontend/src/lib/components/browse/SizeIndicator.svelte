8d393e8b7cdd502b8e3b1fd257a2c2db3df5dac2
mrjleo <mrjleo@users.noreply.github.com>
2025-07-23 13:15:16 +0200

    fix SizeIndicator for total==0

--- frontend/src/lib/components/browse/SizeIndicator.svelte
+++ frontend/src/lib/components/browse/SizeIndicator.svelte
@@ -13,6 +13,9 @@
   }
 
   const { value, total, desc, isEstimate = false }: Props = $props();
+
+  // avoid a division by 0
+  const pct = total == 0 ? 100 : (value / total) * 100;
 </script>
 
 <!--
@@ -27,7 +30,7 @@ A visual indicator of a fraction in relation to the total.
   data-tip="estimated value">
   <div
     class="radial-progress border-3 border-neutral bg-neutral text-primary"
-    style="--size:4.5em; --value:{(value / total) * 100}; --thickness: 0.25em;">
+    style="--size:4.5em; --value:{pct}; --thickness: 0.25em;">
     <span class="text-lg">{toHumanReadable(value)}</span>
   </div>
   <span class="text-xs">{desc}</span>
