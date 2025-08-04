ef0863a7c163a8ec7c83440fe1587cccf1fed52e
mrjleo <mrjleo@users.noreply.github.com>
2025-08-04 18:33:36 +0200

    implement a simple RAG endpoint

--- backend/app/llm/util.py
+++ backend/app/llm/util.py
@@ -1,4 +1,5 @@
 import os
+from collections.abc import Sequence
 from string import Formatter
 
 LLM_PROMPT_SUMMARY = os.environ.get("LLM_PROMPT_SUMMARY")
@@ -26,3 +27,32 @@ def get_summary_prompt(text: str, title: str | None) -> str:
         raise RuntimeError("LLM summary prompt template is malformed.")
 
     return LLM_PROMPT_SUMMARY.format(text=text, title=title)
+
+
+def get_rag_prompt(q: str, documents: Sequence[tuple[str | None, str]]) -> str:
+    """Assemble the LLM input string for RAG.
+
+    :param q: The query/question.
+    :param documents: Titles and texts of documents.
+    :return: The LLM input.
+    """
+    doc_prompt_tpl = """---------- DOCUMENT {docno}
+    Title: {title}
+
+    Text: {text}
+    ---------- END: DOCUMENT {docno}
+    """
+
+    prompt_tpl = """Answer the following question given the context documents below.
+
+    Question: {question}
+
+    Context documents:
+
+    """
+
+    prompt = prompt_tpl.format(question=q)
+    for i, (title, text) in enumerate(documents):
+        prompt += doc_prompt_tpl.format(docno=i + 1, title=title, text=text)
+
+    return prompt
