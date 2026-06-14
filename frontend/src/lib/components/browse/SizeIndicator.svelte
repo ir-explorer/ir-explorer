4f3d4930714daa6096c0e5ede9b3c7da78f0ede6
mrjleo <mrjleo@users.noreply.github.com>
2026-06-14 11:00:22 +0000

    update SizeIndicator style

--- frontend/src/lib/components/browse/SizeIndicator.svelte
+++ frontend/src/lib/components/browse/SizeIndicator.svelte
@@ -24,14 +24,20 @@ A visual indicator of a fraction in relation to the total.
 -->
 <div
   class={[
-    "flex flex-col gap-1.5 text-center",
+    "flex w-24 flex-col text-right",
     isEstimate && "tooltip tooltip-info",
   ]}
   data-tip="estimated value">
-  <div
-    class="radial-progress border-3 border-base-300 bg-base-100 text-primary"
-    style="--size:4.5em; --value:{pct}; --thickness: 0.25em;">
-    <span class="text-lg">{toHumanReadable(value)}</span>
+  <div>
+    <span class="block text-base leading-5 font-semibold text-base-content">
+      {toHumanReadable(value)}
+    </span>
+    <span class="block text-xs leading-3 text-base-content/60">{desc}</span>
   </div>
-  <span class="text-xs text-base-content">{desc}</span>
+  <progress
+    class="progress mt-2 h-1 w-full bg-base-300 progress-primary"
+    value={pct}
+    max="100"
+    aria-label={`${toHumanReadable(value)} ${desc}`}>
+  </progress>
 </div>
