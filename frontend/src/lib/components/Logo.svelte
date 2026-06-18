c448a58549b586c80e74ecdfafb5aada462ae4ff
mrjleo <mrjleo@users.noreply.github.com>
2026-06-18 08:23:54 +0000

    update logo colors

--- frontend/src/lib/components/Logo.svelte
+++ frontend/src/lib/components/Logo.svelte
@@ -11,22 +11,7 @@
 Render the logo.
 -->
 {#if small}
-  <img src="/logo-sm.svg" alt="IR explorer" class="irx-logo h-6" />
+  <img src="/logo-sm.svg" alt="IR explorer" class="h-6 w-auto" />
 {:else}
-  <img
-    src="/logo.svg"
-    alt="IR explorer"
-    class="irx-logo h-16 md:h-20 lg:h-24" />
+  <img src="/logo.svg" alt="IR explorer" class="h-16 w-auto md:h-20 lg:h-24" />
 {/if}
-
-<style>
-  :global(html[data-theme="irx-dark"]) .irx-logo {
-    filter: invert(1) hue-rotate(180deg);
-  }
-
-  @media (prefers-color-scheme: dark) {
-    :global(html:not([data-theme])) .irx-logo {
-      filter: invert(1) hue-rotate(180deg);
-    }
-  }
-</style>
